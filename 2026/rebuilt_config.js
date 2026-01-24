30295255754ee60cb34aa483f0408f65e5a71685
bee-mtlg <shleydabee@gmail.com>
2026-01-23 21:59:06 -0500

    Update rebuilt_config.js

--- 2026/rebuilt_config.js
+++ 2026/rebuilt_config.js
@@ -54,19 +54,6 @@ var config_data = `
       "type": "team",
       "min": 1,
       "max": 99999
-    },
-    { "name": "Auto Start Location",
-      "code": "as",
-      "type": "clickable_image",
-      "filename": "2026/half_field.png",
-      "clickRestriction": "one",
-      "dimensions": "7 10",
-      "allowableResponses": "4 11 18 25 32 39 46 53 60 67",
-      "shape": "circle 5 black red true"
-    },
-    { "name": "Pickup from Neutral Zone",
-      "code": "aff",
-      "type": "bool"
     }
   ],
   "auton": [
