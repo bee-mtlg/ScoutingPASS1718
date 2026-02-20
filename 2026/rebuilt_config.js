fef57250b5b17d3cc4753946369887ca59f087f6
bee-mtlg <shleydabee@gmail.com>
2026-02-20 18:45:51 -0500

    ORIGINAL

--- 2026/rebuilt_config.js
+++ 2026/rebuilt_config.js
@@ -12,6 +12,30 @@ var config_data = `
       "maxSize": 5,
       "required": "true"
     },
+    { "name": "Event",
+      "code": "e",
+      "type": "event",
+      "defaultValue": "2026ilch",
+      "required": "true"
+    },
+    { "name": "Match Level",
+      "code": "l",
+      "type": "level",
+      "choices": {
+        "qm": "Quals<br>",
+        "sf": "Semifinals<br>",
+        "f": "Finals"
+      },
+      "defaultValue": "qm",
+      "required": "true"
+    },
+    { "name": "Match #",
+      "code": "m",
+      "type": "match",
+      "min": 1,
+      "max": 150,
+      "required": "true"
+    },
     { "name": "Robot",
       "code": "r",
       "type": "robot",
@@ -25,23 +49,81 @@ var config_data = `
       },
       "required": "true"
     },
-    { "name": "Match #",
-      "code": "m",
-      "type": "match",
-      "min": 1,
-      "max": 999
-      },
     { "name": "Team #",
       "code": "t",
       "type": "team",
       "min": 1,
       "max": 99999
+    },
+    { "name": "Auto Start Location",
+      "code": "as",
+      "type": "clickable_image",
+      "filename": "2026/half_field.png",
+      "clickRestriction": "one",
+      "dimensions": "7 10",
+      "allowableResponses": "4 11 18 25 32 39 46 53 60 67",
+      "shape": "circle 5 black red true"
     }
   ],
   "auton": [
-    
+    { "name": "Auto Shooting Location",
+      "code": "asl",
+      "type": "clickable_image",
+      "filename": "2026/half_field.png",
+      "dimensions": "7 10",
+      "allowableResponses": "1 2 3 4 8 9 10 11 15 16 17 18 22 23 24 25 29 30 31 32 36 37 38 39 43 44 45 46 50 51 52 53 57 58 59 60 64 65 66 67",
+      "expectedMax": 5,
+      "shape": "circle 5 black red true"
+    },
+    { "name": "Fuel Scored",
+      "code": "afs",
+      "expectedMax": 32,
+      "altInc1": 10,
+      "altInc2": 5,
+      "type": "counter"
+    },
+    { "name": "Pass from Neutral Zone",
+      "code": "apn",
+      "expectedMax": 60,
+      "altInc1": 10,
+      "altInc2": 5,
+      "type": "counter"
+      "code": "tfd",
+      "type": "bool"
+    },
+    { "name": "Climb (L1)",
+      "code": "ac",
+      "type": "radio",
+      "choices": {
+        "c": "Climbed<br>",
+        "a": "Attempted<br>",
+        "x": "Not Attempted"
+      },
+      "defaultValue": "x"
+    },
+    { "name": "Pickup from Depot",
+      "code": "afd",
+      "type": "bool"
+    },
+    { "name": "Pickup from Outpost",
+      "code": "afo",
+      "type": "bool"
+    },
+    { "name": "Pickup from Neutral Zone",
+      "code": "aff",
+      "type": "bool"
+    }
   ],
   "teleop": [
+    { "name": "Shooting Locations",
+      "code": "tsl",
+      "type": "clickable_image",
+      "filename": "2026/half_field.png",
+      "dimensions": "7 10",
+      "allowableResponses": "1 2 3 4 8 9 10 11 15 16 17 18 22 23 24 25 29 30 31 32 36 37 38 39 43 44 45 46 50 51 52 53 57 58 59 60 64 65 66 67",
+      "expectedMax": 25,
+      "shape": "circle 5 black red true"
+    },
     { "name": "Fuel Scored",
       "code": "tfs",
       "expectedMax": 150,
@@ -51,10 +133,20 @@ var config_data = `
     },
     { "name": "Pass from Neutral Zone",
       "code": "pnz",
+      "expectedMax": 250,
+      "altInc1": 10,
+      "altInc2": 5,
+      "type": "counter"
+      "code": "tfd",
       "type": "bool"
     },
     { "name": "Pass from Opp Alliance Zone",
       "code": "poa",
+      "expectedMax": 250,
+      "altInc1": 10,
+      "altInc2": 5,
+      "type": "counter"
+      "code": "tfd",
       "type": "bool"
     },
     { "name": "Pickup from Depot",
@@ -85,14 +177,40 @@ var config_data = `
     }
   ],
   "postmatch": [
+    { "name": "Driver Skill",
+      "code": "ds",
+      "type": "radio",
+      "choices": {
+        "n": "Not Effective<br>",
+        "a": "Average<br>",
+        "v": "Very Effective<br>",
+        "x": "Not Observed"
+      },
+      "defaultValue": "x"
+    },
     { "name": "Defense Rating",
       "code": "dr",
       "type": "radio",
       "choices": {
-        "b": "Played<br>",
+        "b": "Below Average<br>",
+        "a": "Average<br>",
+        "g": "Good<br>",
+        "e": "Excellent<br>",
         "x": "Did not play defense"
       },
       "defaultValue": "x"
+    },
+    { "name": "Speed Rating",
+      "code": "sr",
+      "type": "radio",
+      "choices": {
+        "1": "1 (slow)<br>",
+        "2": "2<br>",
+        "3": "3<br>",
+        "4": "4<br>",
+        "5": "5 (fast)"
+      },
+      "defaultValue":"3"
     },
     { "name": "Crossed Bump",
       "code": "bmp",
@@ -109,6 +227,32 @@ var config_data = `
     { "name": "Tippy<br>(almost tipped over)",
       "code": "tip",
       "type": "bool"
+    },
+    { "name": "Make good<br>alliance partner?",
+      "tooltip": "Would you want this robot on your alliance in eliminations?",
+      "code": "all",
+      "type": "bool"
+    },
+    { "name": "Was Defended",
+      "code": "def",
+      "type": "bool"
+    },
+    { "name": "Excessive Penalties",
+      "code": "pen",
+      "type": "bool"
+    },
+    { "name": "Fuel Percentage",
+      "tooltip": "What percentage of the total fuel for this alliance did this robot score?",
+      "code": "pct",
+      "type": "number",
+      "min": 0,
+      "max": 100
+    },
+    { "name": "Comments",
+      "code": "co",
+      "type": "text",
+      "size": 15,
+      "maxSize": 55
     }
   ]
 }`;
